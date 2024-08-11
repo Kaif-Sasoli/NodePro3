52ff44849f43510b12f555918c3ea0bc200cb1d6
Kaif-Sasoli <158199975+Kaif-Sasoli@users.noreply.github.com>
2024-08-11 11:03:13 +0500

    Update generateToken.js
    Cookies expiry data is extended

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,6 +5,6 @@ const JWT = require('jsonwebtoken');
 // Function for Generating Token
 const generateToken = (user) => {
                                                         // Secret Key
-    return JWT.sign({ email: user.email, id: user._id}, process.env.JWT_KEY, { expiresIn: '1h' });
+    return JWT.sign({ email: user.email, id: user._id}, process.env.JWT_KEY, { expiresIn: '1d' });
 }
 module.exports.generateToken = generateToken;
